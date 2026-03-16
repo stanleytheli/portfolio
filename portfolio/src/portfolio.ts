a284d8b04ef4d1d71185f3eb55d7c62789a02bf5
stanley <stanleytheli@gmail.com>
2026-03-16 15:17:47 -0700

    css cleanup

--- portfolio/src/portfolio.ts
+++ portfolio/src/portfolio.ts
@@ -44,7 +44,7 @@ export function initPortfolio(): void {
           <div class="section-content">
             <article class="experience">
               <div class="experience-header">
-                <img src="https://pngimg.com/uploads/microsoft/microsoft_PNG13.png" alt="Company logo" class="experience-logo">
+                <img src="/msft_logo.png" alt="Company logo" class="experience-logo">
                 <div class="experience-info">
                   <h3 class="experience-title">Microsoft</h3>
                   <span class="experience-company">Software Engineer Intern</span>
@@ -56,7 +56,7 @@ export function initPortfolio(): void {
 
             <article class="experience">
               <div class="experience-header">
-                <img src="https://media.licdn.com/dms/image/v2/C4E0BAQFfc1NOEp9DgA/company-logo_100_100/company-logo_100_100/0/1640018826417/lawrence_berkeley_national_laboratory_logo?e=1775088000&v=beta&t=gqHaPeE1K2NiIAf5EaGB1EC_Tnl_4bTgaVlVj6K0e9s" alt="Company logo" class="experience-logo">
+                <img src="/lbnl_logo.jpg" alt="Company logo" class="experience-logo">
                 <div class="experience-info">
                   <h3 class="experience-title">Lawrence Berkeley Lab</h3>
                   <span class="experience-company">Undergraduate Researcher</span>
@@ -69,7 +69,7 @@ export function initPortfolio(): void {
 
             <article class="experience">
               <div class="experience-header">
-                <img src="https://media.licdn.com/dms/image/v2/D4E0BAQE4FugdFAOu1w/company-logo_100_100/B4EZv8IRHXJAAQ-/0/1769461584805/thumo_logo?e=1775088000&v=beta&t=11RqXKff10kfBdRCahDFVvTqlfxzMyCHfftPajcIiNg" alt="Company logo" class="experience-logo">
+                <img src="/swoop_logo.jpg" alt="Company logo" class="experience-logo">
                 <div class="experience-info">
                   <h3 class="experience-title">Swoop</h3>
                   <span class="experience-company">Software Engineer Intern</span>
@@ -82,7 +82,7 @@ export function initPortfolio(): void {
 
             <article class="experience">
               <div class="experience-header">
-                <img src="https://sp26.datastructur.es/assets/images/bee.webp" alt="Company logo" class="experience-logo">
+                <img src="/cs61b_logo.webp" alt="Company logo" class="experience-logo">
                 <div class="experience-info">
                   <h3 class="experience-title">Tutor</h3>
                   <span class="experience-company">UC Berkeley</span>
@@ -105,7 +105,7 @@ export function initPortfolio(): void {
               <br><br>
               Achieved average solve length of 21.1 (Optimal solvers achieve about 17.7). 
               </p>
-              <img src="https://raw.githubusercontent.com/stanleytheli/cubegpt/refs/heads/main/images/scrambledArrowSolved.png" alt="Gravity Simulation screenshot" class="project-image">
+              <img src="/cubegpt_image.png" alt="Gravity Simulation screenshot" class="project-image">
             </article>
 
             <article class="project">
@@ -113,7 +113,7 @@ export function initPortfolio(): void {
               <p class="project-desc">An ML library built from scratch using only NumPy and SciPy. Derived and implemented custom backprop engine. Implemented DNNs/CNNs/ResNets architectures, RMSProp/Adam optimizers, saving/loading models, multiple loss functions, multiple data augmentation methods, etc. 
               <br><br>
               Image shown: Trained an autoencoder to reduce MNIST digits. These are images interpolated in encoded space, and they mix coherently instead of just blending pixels!</p>
-              <img src="https://media.discordapp.net/attachments/501162564013522956/1412318902301622393/image.png?ex=69b847b9&is=69b6f639&hm=234d8c6379b5be71d00e6072203dbb583cab19e13e392ad71f038d0ab1f02a4e&=&format=webp&quality=lossless&width=1035&height=591" alt="Project Two screenshot" class="project-image">
+              <img src="/autoencoder_result.webp" alt="Project Two screenshot" class="project-image">
             </article>
 
           </div>
